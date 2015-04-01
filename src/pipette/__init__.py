6b747fe02bb48bb7139eba7f6898164375f50cc9
Yauhen Yakimovich <eugeny.yakimovitch@gmail.com>
2015-04-01 13:50:06 +0200

    modified:   src/pipette/__init__.py

--- src/pipette/__init__.py
+++ src/pipette/__init__.py
@@ -9,5 +9,5 @@ Copyright (c) 2014-2015 Yauhen Yakimovich
 Licensed under the MIT License (MIT). Read a copy of LICENSE distributed with
 this code.
 '''
-pipette_version = '0.1.7'
+pipette_version = '0.1.8'
 __version__ = pipette_version
